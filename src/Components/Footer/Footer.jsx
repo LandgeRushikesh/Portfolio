c6e920238d0955d8fcefa03d263365ce15896717
LandgeRushikesh <rushilandge12@gmail.com>
2025-06-07 17:07:54 +0530

    small change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
         <div className="footer-content">
             <h3>The future belongs to those who embrace innovation and never stop learning.</h3>
-            <p>&copy; 2024 Rushikesh Landge | All Rights Reserved</p>
+            <p>&copy; 2025 Rushikesh Landge | All Rights Reserved</p>
         </div>
     </footer>
   )
